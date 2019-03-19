546ede4df4fa121fd6bfc834198326814c9e2fde
yanshao <yanshao@users.noreply.github.com>
2019-03-19 10:27:13 +0800

    更改导出比较大的应用出现的问题
    新开线程去做复制文件的操作

--- app/src/main/java/com/yanshao/extractapk/MainActivity.java
+++ app/src/main/java/com/yanshao/extractapk/MainActivity.java
@@ -1,10 +1,13 @@
 package com.yanshao.extractapk;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,7 +46,21 @@ public class MainActivity extends AppCompatActivity {
     AppListRecyclerAdapter appListRecyclerAdapter;
     ClearEditText key_edit;
     ArrayList<AppInfo> adapterappList = new ArrayList<AppInfo>();
-TextView number_text;
+
+    String apkpath, apkname;
+    TextView number_text;
+    File sd = new File(Environment.getExternalStorageDirectory().getPath() + "/yan");
+
+    ProgressDialog progressDialog;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            copyFile(apkpath, sd.getAbsolutePath(), apkname + ".apk");
+            //需要处理操作
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,7 +88,7 @@ TextView number_text;
                     }
                 }
                 adapterappList.addAll(appList);
-                number_text.setText("共有"+appList.size()+"个应用");
+                number_text.setText("共有" + appList.size() + "个应用");
                 LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
 
                 layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -92,12 +109,18 @@ TextView number_text;
         appListRecyclerAdapter.setOnItemCallClickListener(new AppListRecyclerAdapter.OnItemCallClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                File sd = new File(Environment.getExternalStorageDirectory().getPath() + "/yan");
+
                 ApplicationInfo applicationInfo = null;
                 try {
                     applicationInfo = getPackageManager().getApplicationInfo(appList.get(position).getPackageName(), 0);
                     Log.e("yy", "applicationInfo=" + applicationInfo.sourceDir);
-                    copyFile(applicationInfo.sourceDir, sd.getAbsolutePath(), appList.get(position).getAppName() + ".apk");
+                    apkpath = applicationInfo.sourceDir;
+                    apkname = appList.get(position).getAppName();
+                    if (!TextUtils.isEmpty(apkpath) && !TextUtils.isEmpty(apkname)) {
+                        showWaiting();
+                        new Thread(runnable).start();
+                    }
+
 
                 } catch (PackageManager.NameNotFoundException e) {
                     e.printStackTrace();
@@ -129,7 +152,7 @@ TextView number_text;
 
                 adapterappList.clear();
                 adapterappList.addAll(appList);
-                number_text.setText("共有"+appList.size()+"个应用");
+                number_text.setText("共有" + appList.size() + "个应用");
                 appListRecyclerAdapter.notifyDataSetChanged();
                 Utils.hideKeyboard(MainActivity.this, key_edit);
             }
@@ -139,12 +162,12 @@ TextView number_text;
     private void getkeyList(String key) {
         adapterappList.clear();
 
-        for (int  i=0;i<appList.size();i++){
-            if (appList.get(i).getAppName().indexOf(key)!=-1){
+        for (int i = 0; i < appList.size(); i++) {
+            if (appList.get(i).getAppName().indexOf(key) != -1) {
                 adapterappList.add(appList.get(i));
             }
         }
-        number_text.setText("搜索出"+adapterappList.size()+"个应用");
+        number_text.setText("搜索出" + adapterappList.size() + "个应用");
         appListRecyclerAdapter.notifyDataSetChanged();
     }
 
@@ -155,7 +178,6 @@ TextView number_text;
         PermissionsUtils.getInstance().onRequestPermissionsResult(this, requestCode, permissions, grantResults);
     }
 
-    
 
     public void copyFile(String oldPath, String newPath, String name) {
         try {
@@ -173,13 +195,19 @@ TextView number_text;
                 File path = new File(newfile.getAbsoluteFile() + "/" + name);
                 FileOutputStream fs = new FileOutputStream(path);
                 byte[] buffer = new byte[1444];
+                Log.e("yy", "fs" + ((FileInputStream) inStream).getChannel().size());
                 int length;
                 while ((byteread = inStream.read(buffer)) != -1) {
                     bytesum += byteread; //字节数 文件大小
                     fs.write(buffer, 0, byteread);
+                    Log.e("yy", "bytesum==" + bytesum);
                 }
                 inStream.close();
+                Looper.prepare();
+                progressDialog.dismiss();
+
                 Toast.makeText(MainActivity.this, "导出成功！导出目录为：" + path, Toast.LENGTH_SHORT).show();
+                Looper.loop();
             }
         } catch (Exception e) {
             System.out.println("复制单个文件操作出错");
@@ -188,4 +216,16 @@ TextView number_text;
         }
 
     }
+
+ 
+    private void showWaiting() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setIcon(R.mipmap.ic_launcher);
+        progressDialog.setMessage("导出中...");
+        progressDialog.setIndeterminate(true);// 是否形成一个加载动画  true表示不明确加载进度形成转圈动画  false 表示明确加载进度
+        progressDialog.setCancelable(false);//点击返回键或者dialog四周是否关闭dialog  true表示可以关闭 false表示不可关闭
+        progressDialog.show();
+
+    }
+
 }
